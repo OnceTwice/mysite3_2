931b97820262e2def2b3630070008c6497b52ab4
bit-user <bit-user@bit>
2016-11-01 17:42:16 +0900

    161101

---
+++ src/com/bit2016/mysite/action/guestbook/GuestbookActionFactory.java
@@ -0,0 +1,30 @@
+package com.bit2016.mysite.action.guestbook;
+
+import com.bit2016.web.Action;
+import com.bit2016.web.ActionFactory;
+
+public class GuestbookActionFactory extends ActionFactory {
+
+	@Override
+	public Action getAction(String actionName) {
+		
+		Action action = null;
+		
+		if( "add".equals( actionName ) ) {
+			action = new AddAction();
+		} else if( "deleteform".equals( actionName ) ) {
+			action = new DeleteFormAction();
+		} else if( "delete".equals( actionName ) ) {
+			action = new DeleteAction();
+		} else if( "ajax".equals( actionName ) ) {
+			action = new AjaxAction();
+		} else if( "ajax-list".equals( actionName ) ) {
+			action = new AjaxListAction();
+		} else {
+			action = new ListAction();
+		}
+		
+		return action;
+	}
+
+}
